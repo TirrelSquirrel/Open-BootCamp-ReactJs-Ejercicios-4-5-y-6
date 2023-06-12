68537729d3b0afc185a82abe8c131d953481ef6c
BauteNonSinger <bautista19298@gmail.com>
2023-06-12 11:00:53 +0200

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ClockClase></ClockClase>
       </header>
       <div>
         <Clock nombre='Baute'></Clock>
